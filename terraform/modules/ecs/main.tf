83cacfe5a0f8a5d4af753dba5e09063b177fa261
bcharletto <brunocharletto@gmail.com>
2025-11-20 21:29:23 -0300

    cambios

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -1,25 +1,22 @@
-# main.tf (CORRECT)
-
-# 1. The 'terraform' block for global settings (like provider requirements)
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      # CORREGIDO: Ahora permite versiones 6.x
       version = "~> 6.0" 
     }
   }
 }
 
-# 2. Other top-level blocks like 'provider' (if not already defined)
 provider "aws" {
   region = "us-east-1"
 }
 
-# --- This is the key change ---
-# 3. The 'module' block must be a top-level block, outside of 'terraform'
+data "aws_iam_role" "lab_role" {
+name = "LabRole"
+}
+
+
 module "ecs_fargate_app" {
-  source  = "terraform-aws-modules/ecs/aws" # Fuente EXTERNA de Terraform Registry
+  source  = "terraform-aws-modules/ecs/aws" 
   version = "6.9.0"
-  # ...
 }
