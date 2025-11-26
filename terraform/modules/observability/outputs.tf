4771e40f0152c99d8a6bd0455627d041c5a59b43
constanzaesquivel <cotiesquiveln@gmail.com>
2025-11-25 23:59:54 -0300

    se borra un output que ya no se usa

--- terraform/modules/observability/outputs.tf
+++ terraform/modules/observability/outputs.tf
@@ -2,8 +2,3 @@ output "dashboard_name" {
   value       = aws_cloudwatch_dashboard.stockwiz.dashboard_name
   description = "Nombre del dashboard"
 }
-
-output "log_groups" {
-  value       = [for k, lg in aws_cloudwatch_log_group.service : lg.name]
-  description = "Log groups creados"
-}
