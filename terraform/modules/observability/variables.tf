204bae1cd8c2a1b8151fd3ac0b2db57f391c1d49
constanzaesquivel <cotiesquiveln@gmail.com>
2025-11-27 20:12:10 -0300

    arreglo un archivo con cosas repetidas

--- terraform/modules/observability/variables.tf
+++ terraform/modules/observability/variables.tf
@@ -2,18 +2,5 @@ variable "region" { type = string }
 variable "env"    { type = string }
 variable "ecs_cluster_name" { type = string }
 variable "ecs_service_name" {type = string }
-
-variable "alb_arn_suffix" {
-  type        = string
-  default     = ""
-}
-
+variable "alb_arn_suffix" { type = string }
 variable "target_group_arn_suffix" {type = string }
-
-variable "ecs_cluster_name" {
-  type        = string
-}
-
-variable "alb_arn_suffix" {
-  type        = string
-}
