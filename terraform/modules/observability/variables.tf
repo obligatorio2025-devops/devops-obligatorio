f868b8ec2a994a7a85f8ff24489e0a1e66f4e651
Constanza Esquivel <106634239+constanzaesquivel@users.noreply.github.com>
2025-11-27 19:17:42 -0300

    agrego type

--- terraform/modules/observability/variables.tf
+++ terraform/modules/observability/variables.tf
@@ -1,11 +1,11 @@
 variable "region" { type = string }
 variable "env"    { type = string }
 variable "ecs_cluster_name" { type = string }
-variable "ecs_service_name" {}
+variable "ecs_service_name" {type = string }
 
 variable "alb_arn_suffix" {
   type        = string
   default     = ""
 }
 
-variable "target_group_arn_suffix" {}
+variable "target_group_arn_suffix" {type = string }
